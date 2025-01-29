87bfd33f3cc2dced0a25558bc1d3ce5c829668ea
Tanner <tannerrosslivingston@gmail.com>
2025-01-28 21:30:45 -0700

    Add RCON support
    * Approved discord users can send RCON commands via discord messages
    * Other internal refactors

--- src/rcon.ts
+++ src/rcon.ts
@@ -1,16 +1,28 @@
 import Rcon from 'rcon-srcds';
 import { logError, logMessage } from './utils';
+import { RCON_CONFIG } from './config';
 
-const pass = "pass";
-const rconServer = new Rcon({ host: 'ip', port: 25575 })
+const pass = RCON_CONFIG.RCON_PASSWORD;
+const host = RCON_CONFIG.RCON_HOST;
+const port = RCON_CONFIG.RCON_PORT;
 
-export async function authenticateRconServer() {
+let rconServer: Rcon;
+
+export async function authenticateRconServer(): Promise<boolean> {
+    if (!host || !port || !pass) {
+        logMessage('RCON server not configured');
+        return false;
+    }
+    
+    rconServer = new Rcon({ host, port });
+
     try {
         await rconServer.authenticate(pass);
         logMessage('Authenticated RCON server');
+        return true;
     } catch (error) {
         logError('Failed to authenticate RCON server', error);
-        throw error;
+        return false;
     }
 }
 
